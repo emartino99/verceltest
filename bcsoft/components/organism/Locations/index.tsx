10e1bfd39a39b22c70328513ac714c49f67807f8
Salvatore Federico <salvatore.caruso@bcsoft.net>
2022-07-01 14:40:23 +0200

    feat(develop) partners webparts completed

--- bcsoft/components/organism/Locations/index.tsx
+++ bcsoft/components/organism/Locations/index.tsx
@@ -39,5 +39,3 @@ export function Locations() {
     </section>
   );
 };
-
-export default Locations;
--- bcsoft/components/organism/Partners/index.tsx
+++ bcsoft/components/organism/Partners/index.tsx
@@ -1,5 +1,6 @@
 import { usePartnersHook } from '../../../services/partners';
 import { getRelativePath } from '../../../utils';
+
 export function Partners() {
 
     const { 
@@ -7,31 +8,26 @@ export function Partners() {
         error
     } = usePartnersHook();
 
-    console.log("partne", partners);
-
     return (
-        <div  className='section d-flex flex-column justify-content-center align-items-center py-4'>
-            <h1>PARTNERS</h1>
-            {   partners?.map((item, i) => {
-                const pari = i % 2 === 0;
-                const direction = pari ? 'start' : 'end';
-                const reverseDirection = pari ? 'end' : 'start';
-                console.log("ciao", getRelativePath(item.PartnerIMG))
-            return(
-              <div key={item.Title} className='d-flex flex-wrap justify-content-center align-items-center w-100' style={{flexDirection: pari ? 'row' : 'row-reverse', gap: '2rem'}}>
-                <div className={`d-flex justify-content-${reverseDirection}`} style={{flex: 1}}>
-                  <h2 className='bold text-center text-md-end' dangerouslySetInnerHTML={{__html: item.Title}}></h2>
-                </div>
-                <div className={`d-flex justify-content-${direction}`} style={{flex: 1, backgroundColor: "trasparent"}}>
-                  <div>
-                    {item.PartnerIMG && <img src={`${process.env.NEXT_PUBLIC_BASE_URL_ASSETS}${getRelativePath(item.PartnerIMG)}`} alt={item.Title} className='' />}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+        <div className='partners span-1-12'>
+            {
+                partners?.map((item, i) => {
+                    return(
+                        <div key={item.ID} className='partners-card' style={{flexDirection: `${i % 2 === 0 ? 'row' : 'row-reverse'}` }}>
+                            <div className='partners-card-title' style={{ justifyContent: `${i % 2 === 0 ? 'flex-end' : 'flex-start'}` }} >
+                                <h2 dangerouslySetInnerHTML={{__html: item.Title}}></h2>
+                            </div>
+                            <div className='partners-card-image' style={{justifyContent: `${i % 2 === 0 ? 'flex-start' : 'flex-end'}`}} >
+                                <div className='partners-card-image-container'>
+                                    <div>
+                                        <img src={`${process.env.NEXT_PUBLIC_BASE_URL_ASSETS}${getRelativePath(item.PartnerIMG)}`} alt={item.Title} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
-      );
+    );
 };
-
-export default Partners;
