1a84d1d1a7e8a8bd0e81239c64d7a3928df71305
PieroProcessing <piero.dauber@gmail.com>
2022-07-01 12:54:00 +0200

    feat(util): getMediaPath

--- bcsoft/utils/parseImg.ts
+++ bcsoft/utils/parseImg.ts
@@ -14,3 +14,5 @@ export const getRelativePath =   (item:string): string =>{
       const parsed = JSON.parse(item);
     return parsed.serverRelativeUrl
 }
+
+export const getMediaPath = (item: string) => `${process.env.NEXT_PUBLIC_BASE_URL_ASSETS}${getRelativePath(item)}`
