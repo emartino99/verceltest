a12a55e4c68bf4b3a0c4d89eb6d9eaed20a3556e
Salvatore Federico <salvatore.caruso@bcsoft.net>
2022-07-22 11:44:31 +0200

    feat(design): created page designAndCommunication

---
+++ bcsoft/models/design-communication.ts
@@ -0,0 +1,14 @@
+export interface IDesignCommunication {
+    ID: number;
+    Title: string;
+}
+
+export interface IDesignCommunicationMainSettings {
+    ID: number;
+    Title: string;
+    designImage: string;
+    communicationImage: string;
+    image: string;
+    buttonLabel: string;
+    buttonHref: string;
+}
